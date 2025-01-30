cb389001105f98d9bf2407bdeb3184c67b416d6f
blzaa <bruno.ctba@hotmail.com>
2025-01-30 18:45:38 -0300

    update

--- teste2.php
+++ teste2.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
-    <body class="bg-light">
+    <body class="bg-dark">
 
     <div class="container mt-5">
             <h1 class="text-center mb-4">Calculadora na prática 2</h1>
