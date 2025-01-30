93034541cbd2608707a277341be5cb237b05b9d6
blzaa <bruno.ctba@hotmail.com>
2025-01-30 18:34:44 -0300

    teste2

--- teste.php
+++ teste.php
@@ -4,28 +4,13 @@
         <meta charset="UTF-8">
         <title>Calculadora 2</title>
 
-    <script>
-        function validarFormulario(){
-            const num1 = document.getElementById('num1').value;
-            const num2 = document.getElementById('num2').value;
-            const operacao = document.getElementById('operacao').value;
-
-            if (num1 === '' || num2 ==='') {
-                alert('Preencha todos os campos!');
-                return false;
-            }
-
-            if (operacao === 'divisao' && num1 == '0' || num2 == '0') {
-                alert('Divisão por zero, é ZERO!')
-                return false;
-            }}
-    </script>
+    <script src="operation.js"></script>
     </head>
 
     <body>
         <h1>Calculadora na prática</h1>
 
-        <form action="teste.php" method="POST" onsubmit="return validarFormulario()">
+        <form action="teste.php" method="POST" onsubmit="return valForm()">
             <label for="num1">Número 1:</label>
             <input type="number" id="num1" name="num1"><br><br>
 
@@ -44,31 +29,8 @@
         </form>
 
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $operacao = $_POST['operacao'];
-
-    switch ($operacao) {
-        case 'soma':
-            $resultado = $num1 + $num2;
-            break;
-        case 'subtracao':
-            $resultado = $num1 - $num2;
-            break;
-        case 'multiplicacao':
-            $resultado = $num1 * $num2;
-            break;
-        case 'divisao':
-            $resultado = $num1 / $num2;
-            break;
-    }
-
-    echo "<h2>O resultado é: $resultado</h2>";
-
-}
-
+include "form.php"
 ?>
+
     </body>
 </html>
